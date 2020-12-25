5c4053af215a3be4f93ac176d806451f4cce818e
sandeshll <66826047+sandeshll@users.noreply.github.com>
2020-12-25 14:40:17 +0530

    react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=> <PersonList/>;
+
+
+const PersonList = () => {
+const people = [
+  {
+    img:22,
+    name:'john',
+    job:'developer'
+  },
+  {
+    img:75,
+    name:'bob',
+    job:'designer'
+  },
+  {
+    img:77,name:'doe',job:'artist'
+  }
+]
+
+  return (<section>
+  <Person person = {people[0]} />
+  <Person person = {people[1]} >
+    lorem  lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
+  </Person>
+  <Person person = {people[2]} />
+  </section>) 
 }
+
+const Person = (props) =>{
+  const {img, name, job,} = props.person;
+  const {children} = props;
+  const url =
+  `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
+   return (
+      <div className="person">
+     <img src={url} alt="" />
+     <div>
+       <h4>{name}</h4>
+       <h4>{job}</h4>
+       {children}
+     </div>
+   </div>
+   );
+};
+
+
+
+
+
+
 
 export default App;
